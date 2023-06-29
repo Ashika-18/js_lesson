ed4086f98c2c4935f6ec53844d7063daa2d5196f
Ashika-18 <ashibasama@gmail.com>
2023-06-30 06:05:44 +0900

    first-color

--- index.php
+++ index.php
@@ -3,10 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="demo.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <title>カラーピッカー</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="demo.js" defer></script>
 </head>
 <body>
-    <p>これはテストです！</p>
+    <div>
+    <p id="colorText">カラーコードを検索</p>
+    <input id="colorPicker" type="color">
+    </div>
 </body>
 </html>
