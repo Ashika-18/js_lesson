37732acd1ada4b54b2ce0c4ee8af7183f53ac6fa
Ashika-18 <ashibasama@gmail.com>
2023-06-30 20:26:26 +0900

    関数

--- demo.js
+++ demo.js
@@ -1,1 +1,19 @@
-alert("これからJavaScript!")
+const text = document.querySelector('#colorText');
+
+const color = document.querySelector('#colorPicker');
+
+const colorBg = () => {
+
+    text.textContent = `カラーコード:${color.value}`
+}
+ 
+color.addEventListener('input', colorBg);
+
+const message = (name, weather) => {
+
+    return `${name}さま、ご機嫌麗しゅう！本日は${weather}ですよ。`;
+}
+
+console.log(message('Ashika', '大雨'));
+
+alert(message('知子', 'ド晴天'));
